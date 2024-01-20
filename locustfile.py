f43a76c0650efe70f83fd3f4d4041dc62d16455e
frimpong-adotri-01 <67342788+frimpong-adotri-01@users.noreply.github.com>
2024-01-20 16:29:52 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,6 @@ from locust import HttpUser, task
 class SimulatedUser(HttpUser):
     @task
     def send_request(self):
-        #self.client.get("/predict?sepL=5.1&petL=1.4&sepW=3.5&petW=0.1")
         self.client.get("/predict?sepL=5.1&petL=1.4&sepW=3.5&petW=0.1",name="/predict1")
         self.client.get("/predict?sepL=6.4&petL=5.3&sepW=2.3&petW=3.2",name="/predict2")
         self.client.get("/predict?sepL=5.1&petL=1.4&sepW=3.5&petW=0.1",name="/predict3")
